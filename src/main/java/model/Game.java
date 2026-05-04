528ac428c14687939e315ef5bdc221eb953757c1
AidanMott <aidanmott@icloud.com>
2026-05-03 23:58:58 -0500

    G-01 to G-03 pass

--- src/main/java/model/Game.java
+++ src/main/java/model/Game.java
@@ -1,4 +1,33 @@
 package model;
 
 public class Game {
+
+    private Board board;
+    private Player whitePlayer;
+    private Player blackPlayer;
+    private GameState state;
+
+    public Game(String whiteName, String blackName) {
+
+        if (whiteName == null || whiteName.trim().isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        if (blackName == null || blackName.trim().isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        this.whitePlayer = new Player(whiteName, Color.WHITE);
+        this.blackPlayer = new Player(blackName, Color.BLACK);
+
+        this.board = new Board();
+        this.state = new GameState();
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public GameState getState() {
+        return state;
+    }
 }
